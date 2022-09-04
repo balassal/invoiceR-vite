1f366812fa5b2646f9e51a61ead7b97823a2911f
Laszlo Balassa <b.lacika23@gmail.com>
2022-09-04 13:30:02 +0200

    refactor

--- src/pages/invoice/Invoices.vue
+++ src/pages/invoice/Invoices.vue
@@ -2,9 +2,9 @@
   <q-page class="q-pa-sm">
     <div class="q-mt-sm shadow-2">
       <q-table
-        title="Treats"
+        title="Invoices"
         class="invoices-table"
-        :rows="rows"
+        :rows="invoices"
         :columns="columns"
         row-key="id"
         :visible-columns="visibleColumns"
@@ -20,12 +20,6 @@
         <template v-slot:top>
           <div class="q-gutter-xs">
             <q-btn color="primary" icon="add" label="Add" to="/invoice/new" />
-            <q-btn color="secondary" icon="file_download" label="Import" />
-            <q-btn
-              icon="file_upload"
-              label="Export to csv"
-              @click="exportTable"
-            />
           </div>
           <q-space />
           <q-input
@@ -55,11 +49,6 @@
             style="min-width: 150px"
           />
         </template>
-        <!-- <template v-slot:body-cell-name="props">
-          <q-td :props="props"> 
-            <a href="#/invoices/{{props.row.id}}">{{ props.row.name }}</a>
-          </q-td>
-        </template> -->
         <template v-slot:no-data="{ icon, message, filter }">
           <div class="full-width row flex-center q-gutter-sm">
             <q-icon size="2em" name="sentiment_dissatisfied" />
@@ -72,141 +61,143 @@
   </q-page>
 </template>
 
-<script>
-import { useQuasar, exportFile } from "quasar";
-import { defineComponent } from "vue";
-import { ref, computed } from "vue";
-import { getInvoices } from "../../store/invoice";
+<script setup>
+import { computed, ref } from "@vue/reactivity";
+import { useQuasar } from "quasar";
+import { getInvoices } from "src/store/invoice";
+import { onMounted } from "vue";
 import { useRouter } from "vue-router";
+
+const $q = useQuasar();
+const screenHeight = computed(() => $q.screen.height - 80);
+const router = useRouter();
+const loading = ref(false);
+const invoices = ref([]);
+const filter = ref("");
+const selected = ref([]);
 
 const columns = [
   {
     name: "name",
     required: true,
-    label: "Number",
+    label: "Name",
     align: "left",
-    field: (row) => row.name,
-    format: (val) => `${val}`,
+    field: "name",
     sortable: true,
   },
-  { name: "partner", label: "Partner", field: "partner", sortable: true },
-  { name: "date", label: "Date", field: "date", sortable: true },
-  { name: "delivery", label: "Delivery", field: "delivery", sortable: true },
-  { name: "due", label: "Due", field: "due", sortable: true },
-  { name: "payment", label: "Payment", field: "payment", sortable: true },
-  { name: "currency", label: "Currency", field: "currency", sortable: true },
   {
-    name: "netamount",
-    label: "Net Amount",
-    field: "netamount",
+    name: "status",
+    required: true,
+    label: "Status",
+    align: "center",
+    field: "status",
     sortable: true,
   },
-  { name: "vat", label: "VAT", field: "vat", sortable: true },
-  { name: "total", label: "Total", field: "total", sortable: true },
-];
-
-const rows = getInvoices().map((inv) => {
-  return {
-    id: inv.id,
-    name: inv.number,
-    partner: inv.partnerId.name,
-    date: inv.invoiceDate,
-    delivery: inv.deliveryDate,
-    due: inv.dueDate,
-    payment: inv.paymentMode,
-    currency: inv.currencyId.label,
-    netamount: inv.netAmount.toLocaleString(),
-    vat: inv.vatAmount.toLocaleString(),
-    total: inv.totalAmount.toLocaleString(),
-  };
-});
-
-function wrapCsvValue(val, formatFn) {
-  let formatted = formatFn !== void 0 ? formatFn(val) : val;
-
-  formatted =
-    formatted === void 0 || formatted === null ? "" : String(formatted);
-
-  formatted = formatted.split('"').join('""');
-  /**
-   * Excel accepts \n and \r in strings, but some other CSV parsers do not
-   * Uncomment the next two lines to escape new lines
-   */
-  // .split('\n').join('\\n')
-  // .split('\r').join('\\r')
-
-  return `"${formatted}"`;
-}
-
-export default defineComponent({
-  name: "PageInvoices",
-  setup() {
-    const $q = useQuasar();
-    const router = useRouter();
-    const loading = ref(false);
-    const filter = ref("");
-    const screenHeight = computed(() => {
-      return $q.screen.height - 80;
-    });
-    const selected = ref([]);
-    const invoices = ref(getInvoices());
-    return {
-      loading,
-      filter,
-      visibleColumns: ref([
-        "name",
-        "partner",
-        "delivery",
-        "date",
-        "due",
-        "netamount",
-        "vat",
-        "total",
-        "payment",
-        "currency",
-      ]),
-      columns,
-      rows,
-      screenHeight,
-      selected,
-      invoices,
-
-      exportTable() {
-        // naive encoding to csv format
-        const content = [columns.map((col) => wrapCsvValue(col.label))]
-          .concat(
-            rows.map((row) =>
-              columns
-                .map((col) =>
-                  wrapCsvValue(
-                    typeof col.field === "function"
-                      ? col.field(row)
-                      : row[col.field === void 0 ? col.name : col.field],
-                    col.format
-                  )
-                )
-                .join(",")
-            )
-          )
-          .join("\r\n");
-
-        const status = exportFile("table-export.csv", content, "text/csv");
-
-        if (status !== true) {
-          $q.notify({
-            message: "Browser denied file download...",
-            color: "negative",
-            icon: "warning",
-          });
-        }
-      },
-
-      onRowClicked(evt, row) {
-        router.push({ name: "invoice", params: { id: row.id, editable: false} })
-      },
-    };
+  {
+    name: "partnerId",
+    label: "Partner",
+    align: "center",
+    field: (row) => row.partnerId.name,
+    sortable: true,
   },
+  {
+    name: "invoiceDate",
+    required: true,
+    label: "Date",
+    align: "center",
+    field: "invoiceDate",
+    sortable: true,
+  },
+  {
+    name: "deliveryDate",
+    label: "Delivery Date",
+    align: "center",
+    field: "deliveryDate",
+    sortable: true,
+  },
+  {
+    name: "dueDate",
+    label: "Due Date",
+    align: "center",
+    field: "dueDate",
+    sortable: true,
+  },
+  {
+    name: "paymentTherm",
+    label: "Payment Therm",
+    align: "center",
+    field: "paymentTherm",
+    sortable: true,
+  },
+  {
+    name: "paymentMode",
+    label: "Payment Mode",
+    align: "center",
+    field: "paymentMode",
+    sortable: true,
+  },
+  {
+    name: "currencyId",
+    label: "Currency",
+    align: "center",
+    field: (row) => row.currencyId.label,
+    sortable: true,
+  },
+  {
+    name: "netAmount",
+    label: "Net Amount",
+    align: "center",
+    field: "netAmount",
+    format: (val) => `${val.toLocaleString()}`,
+    sortable: true,
+  },
+  {
+    name: "vatAmount",
+    label: "Tax",
+    align: "center",
+    field: "vatAmount",
+    format: (val) => `${val.toLocaleString()}`,
+    sortable: true,
+  },
+  {
+    name: "totalAmount",
+    required: true,
+    label: "Total",
+    align: "center",
+    field: "totalAmount",
+    format: (val) => `${val.toLocaleString()}`,
+    sortable: true,
+  },
+  {
+    name: "toBePaid",
+    label: "Paid",
+    align: "center",
+    field: "toBePaid",
+    format: (val) => `${val.toLocaleString()}`,
+    sortable: true,
+  },
+];
+const visibleColumns = ref([
+  "name",
+  "status",
+  "partnerId",
+  "invoiceDate",
+  "totalAmount",
+]);
+
+onMounted(async () => {
+  await loadInvoices();
 });
+
+const loadInvoices = async () => {
+  loading.value = true;
+  invoices.value = await getInvoices();
+  loading.value = false;
+};
+
+const onRowClicked = (e, row) => {
+  router.push({ name: "invoice", params: { id: row.id } });
+};
 </script>
 
 <style lang="sass">
