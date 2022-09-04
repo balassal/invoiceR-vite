7b6e69532ba0eccbebeac689f78965e6829f76c0
Laszlo Balassa <b.lacika23@gmail.com>
2022-09-04 13:20:43 +0200

    refactor

--- src/components/InvoiceLineDialog.vue
+++ src/components/InvoiceLineDialog.vue
@@ -1,23 +1,28 @@
 <template>
-  <q-dialog v-model="showDialog" persistent>
-    <q-card style="min-width: 75vw">
+  <q-dialog ref="dialogRef" persistent @hide="onDialogHide">
+    <q-card style="width: 75vw; max-width: 75vw">
       <q-card-section>
         <div class="text-h6">{{ props.title }}</div>
       </q-card-section>
 
       <q-separator />
 
-      <q-card-section class="row" style="max-height: 60vh; overflow-y: auto">
+      <q-card-section class="row">
         <div class="col-8">
           <div class="row items-center">
             <div class="col-3">Product</div>
             <div class="col-9">
               <q-select
+                ref="productField"
                 dense
                 clearable
                 :options="products"
                 v-model="state.productId"
                 @update:model-value="onProductChange"
+                autofocus
+                :rules="[(val) => !!val || 'Product is required']"
+                lazy-rules="ondemand"
+                hint="Select a product"
               />
             </div>
           </div>
@@ -27,24 +32,31 @@
               <q-input dense clearable autogrow v-model="state.label" />
             </div>
           </div>
-          <div class="row items-center">
+          <div class="row items-center q-py-sm">
             <div class="col-3">Quantity</div>
             <div class="col-9">
               <div class="row items-center">
                 <div class="col">
                   <q-input
+                    ref="quantityField"
                     dense
                     clearable
+                    class="q-pb-none"
                     type="number"
                     v-model.number="state.quantity"
                     @update:model-value="calculate"
+                    :rules="[
+                      (val) => !!val || 'Quantity is required',
+                      (val) => val != 0 || 'Cannot be zero',
+                    ]"
+                    lazy-rules="ondemand"
                   />
                 </div>
                 <div class="col">
                   <q-input
                     class="q-pl-md"
                     dense
-                    clearable
+                    readonly
                     label="Unit of Measure"
                     v-model="state.uomId"
                   />
@@ -52,19 +64,22 @@
               </div>
             </div>
           </div>
-          <div class="row items-center">
+          <div class="row items-center q-py-sm">
             <div class="col-3">Unit Price</div>
             <div class="col-9">
               <q-input
+                ref="unitPriceField"
                 type="number"
                 dense
                 clearable
-                v-model="state.unitPrice"
+                v-model.number="state.unitPrice"
                 @update:model-value="calculate"
+                :rules="[(val) => !!val || 'Unit Price is required']"
+                lazy-rules="ondemand"
               />
             </div>
           </div>
-          <div class="row items-center">
+          <div class="row items-center q-py-sm">
             <div class="col-3">Taxes</div>
             <div class="col-9">
               <q-select
@@ -109,7 +124,14 @@
 
       <q-card-actions align="right" class="text-primary">
         <q-btn
-          v-if="!edit"
+          flat
+          color="secondary"
+          icon="close"
+          label="Cancel"
+          @click="onDialogCancel"
+        />
+        <q-btn
+          v-if="props.mode === 'add'"
           flat
           label="Add"
           icon="add_circle"
@@ -117,27 +139,20 @@
           @click="addNewLine"
         />
         <q-btn
-          v-if="edit"
+          v-if="props.mode === 'edit'"
           flat
           label="Save"
           icon="save"
-          color="primary"
+          color="orange"
           @click="modifyLine"
         />
         <q-btn
-          v-if="edit"
+          v-if="props.mode === 'edit'"
           flat
           label="Delete"
           color="red"
           icon="delete"
           @click="deleteLine"
-        />
-        <q-btn
-          flat
-          label="Cancel"
-          icon="cancel"
-          color="secondary"
-          @click="showDialog = false"
         />
       </q-card-actions>
     </q-card>
@@ -145,15 +160,17 @@
 </template>
 
 <script setup>
-import { ref, computed, onMounted } from "vue";
+import { ref, onMounted, reactive } from "vue";
+import { useDialogPluginComponent } from "quasar";
 import { getProducts } from "src/store/product";
 import { getTaxes } from "src/store/taxes";
+import { v4 as uuid } from "uuid";
+
+const productField = ref(null);
+const quantityField = ref(null);
+const unitPriceField = ref(null);
 
 const props = defineProps({
-  show: {
-    type: Boolean,
-    required: true,
-  },
   title: {
     type: String,
     required: true,
@@ -161,83 +178,132 @@ const props = defineProps({
   line: {
     type: Object,
   },
-  edit: {
-    type: Boolean,
+  mode: {
+    type: String,
     required: true,
   },
 });
-const emit = defineEmits([
-  "update:show",
-  "onAddNewLine",
-  "onModifyLine",
-  "onDeleteLine",
-]);
 
-const showDialog = computed({
-  get() {
-    return props.show;
-  },
-  set(val) {
-    emit("update:show", val);
-  },
-});
-const edit = computed({
-  get() {
-    return props.edit;
-  },
-});
+const emit = defineEmits([...useDialogPluginComponent.emits]);
 
-const state = computed({
-  get() {
-    return props.line;
-  },
+const { dialogRef, onDialogHide, onDialogOK, onDialogCancel } =
+  useDialogPluginComponent();
+
+const state = reactive({
+  id: "",
+  productId: "",
+  netAmount: 0,
+  label: "",
+  quantity: 0,
+  uomId: "",
+  unitPrice: 0,
+  taxesIds: [],
+  total: 0,
 });
 
 const products = ref(null);
 const taxes = ref(null);
 
-onMounted(() => {
-  products.value = getProducts();
-  taxes.value = getTaxes();
+onMounted(async () => {
+  products.value = await getProducts();
+  taxes.value = await getSaleTaxes();
+
+  if (props.line) {
+    state.id = props.line.id;
+    state.productId = props.line.productId;
+    state.netAmount = props.line.netAmount;
+    state.label = props.line.label;
+    state.quantity = props.line.quantity;
+    state.uomId = props.line.uomId;
+    state.unitPrice = props.line.unitPrice;
+    state.taxesIds = props.line.taxesIds;
+    state.total = props.line.total;
+  }
 });
 
+const getSaleTaxes = async () => {
+  const taxes = await getTaxes();
+  const saleTaxes = taxes.filter((tax) => tax["type"] === "sale");
+  return saleTaxes;
+};
+
 const onProductChange = () => {
-  if (state.value.productId !== null) {
-    state.value.label = `[${state.value.productId.internalref}] ${state.value.productId.label}`;
-    state.value.uomId = state.value.productId.uomId.name;
-    state.value.unitPrice = state.value.productId.unitprice;
-    state.value.taxesIds = state.value.productId.saletaxes;
+  if (state.productId !== null) {
+    state.label = `[${state.productId.internalref}] ${state.productId.label}`;
+    state.uomId = state.productId.uomId.name;
+    state.unitPrice = state.productId.unitprice;
+    state.taxesIds = state.productId.saletaxes;
   }
 };
 
 const calculate = () => {
-  state.value.netAmount = state.value.quantity * state.value.unitPrice;
+  state.netAmount = state.quantity * state.unitPrice;
 
-  let total = state.value.netAmount;
-  if (state.value.taxesIds !== null) {
-    state.value.taxesIds.forEach((tax) => {
-      const current = state.value.netAmount * tax.amount;
+  let total = state.netAmount;
+  if (state.taxesIds !== null) {
+    state.taxesIds.forEach((tax) => {
+      const current = state.netAmount * tax.amount;
       total = total + current;
     });
   }
-  state.value.total = total;
+  state.total = total;
 
-  state.value.taxAmount = total - state.value.netAmount;
+  state.taxAmount = total - state.netAmount;
+};
+
+const isValid = () => {
+  return (
+    productField.value.validate() &&
+    quantityField.value.validate() &&
+    unitPriceField.value.validate()
+  );
 };
 
 const addNewLine = () => {
-  emit("onAddNewLine", JSON.stringify(state.value));
-  showDialog.value = false;
+  if (isValid()) {
+    const newLine = {
+      id: uuid(),
+      label: state.label,
+      quantity: state.quantity,
+      uomId: state.uomId,
+      unitPrice: state.unitPrice,
+      taxesIds: JSON.parse(JSON.stringify(state.taxesIds)),
+      taxAmount: state.taxAmount,
+      netAmount: state.netAmount,
+      total: state.total,
+      productId: JSON.parse(JSON.stringify(state.productId)),
+    };
+    onDialogOK({
+      line: newLine,
+    });
+  }
 };
 
 const modifyLine = () => {
-  emit("onModifyLine", JSON.stringify(state.value));
-  // edit.value = false;
-  showDialog.value = false;
+  if (isValid()) {
+    const modifLine = {
+      id: state.id,
+      label: state.label,
+      quantity: state.quantity,
+      uomId: state.uomId,
+      unitPrice: state.unitPrice,
+      taxesIds: JSON.parse(JSON.stringify(state.taxesIds)),
+      taxAmount: state.taxAmount,
+      netAmount: state.netAmount,
+      total: state.total,
+      productId: JSON.parse(JSON.stringify(state.productId)),
+    };
+    onDialogOK({
+      line: modifLine,
+      type: "update",
+    });
+  }
 };
 
 const deleteLine = () => {
-  emit("onDeleteLine", state.value.id);
-  showDialog.value = false;
+  onDialogOK({
+    id: state.id,
+    type: "delete",
+  });
 };
 </script>
